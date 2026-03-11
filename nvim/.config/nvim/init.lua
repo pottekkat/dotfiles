9f87a1e0eae93b4f3260839b9c8adc5cf5fd49ef
Navendu Pottekkat <navendu@apache.org>
2026-03-11 22:47:55 +0530

    feat: add nvim plugins, keymaps, enable gitsigns keymaps

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -112,6 +112,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Close all buffers except the current one
+vim.keymap.set('n', '<leader>bo', '<cmd>%bd|e#|bd#<CR>', { desc = 'Close all [b]uffers except current ([o]nly)' })
+
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
@@ -925,7 +928,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
--- nvim/.config/nvim/lua/custom/plugins/copilot.lua
+++ nvim/.config/nvim/lua/custom/plugins/copilot.lua
@@ -1,3 +1,21 @@
 return {
   'github/copilot.vim',
+  keys = {
+    {
+      '<leader>tp',
+      function()
+        local copilot_enabled = vim.g.copilot_enabled
+        if copilot_enabled == nil or copilot_enabled then
+          vim.cmd('Copilot disable')
+          vim.g.copilot_enabled = false
+          print('Copilot disabled')
+        else
+          vim.cmd('Copilot enable')
+          vim.g.copilot_enabled = true
+          print('Copilot enabled')
+        end
+      end,
+      desc = '[T]oggle Co[p]ilot'
+    }
+  }
 }
